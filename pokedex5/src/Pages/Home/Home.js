a62c06805e1bb4967e9f0f3c4fae9c832465e5ca
Sabrina Pires <sabrinapires_@outlook.com>
2022-05-03 18:24:27 -0300

    Iniciando requizição

--- pokedex5/src/Pages/Home/Home.js
+++ pokedex5/src/Pages/Home/Home.js
@@ -1,15 +1,49 @@
 import { goToDetailsPage, goToPokedexPage } from "../../routes/coordinator"
 import { useNavigate } from "react-router-dom";
-import React from "react";
+import React, {useEffect, useState} from "react";
+import { CardPokedex } from "../../components/CardPokedex";
+import axios from "axios"
 
 const Home =() => {
     const navigate = useNavigate()
+    const [listPokemon, setListPokemon] = useState([])
 
+    const getPokemons = async () =>{
+        axios
+        .get(`https://pokeapi.co/api/v2/pokemon/1/`)
+        .then((res)=>{
+        setListPokemon(res.data)
+        })
+        .catch((err)=> {
+            alert("Erro! Sem pokemons.", err)
+        })
+    }
+
+    const renderPokemons = listPokemon ? (
+        listPokemon.map((pokemon)=>{
+        return(
+            <CardPokedex pokemon={pokemon}/>
+        )
+    })
+    ) : ( <p> Erro! </p>) 
+
+    // Array.map ((pokemon) =>{
+    //     axios.get(url do res.data${pokemon}
+    //     .then(res.data.sprint))
+    //   })
+
+    console.log(listPokemon);
+
+    useEffect(()=>{
+        getPokemons()
+    }, [])
     return(
         <div>
             <h1>Lista de Pokemons</h1>
             <button onClick={()=> goToPokedexPage(navigate)}>Ir para Pokedex</button>
             <div>
+                <CardPokedex/>
+                {/* {renderPokemons} */}
                 <p>Imagem</p>
                 <button >Adicionar a Pokédex</button>
                 <button onClick={() => goToDetailsPage(navigate)}>Detalhes</button>
---
+++ pokedex5/src/components/CardPokedex.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios"
+
+export const CardPokedex = () =>{
+    const [imagePokemon, setImagePokemom] = useState([])
+    
+    const getImagePokemon = async () =>{
+        axios
+        .get(`https://pokeapi.co/api/v2/pokemon/1/`)
+        .then((res)=>{
+            setImagePokemom(res.data)
+        })
+        .catch((err)=>{
+            alert("Erro! Sem imagens.", err)
+        })
+    }
+
+    useEffect(()=>{
+        getImagePokemon()
+    }, [])
+
+    // const renderImagePokemons= imagePokemon.map((pokemon)=>{
+    //     return(
+    //         <div>
+    //             <p>{pokemon.name}</p>
+    //             <img src={pokemon.official}/>
+    //         </div>
+    //     )
+    // })
+    console.log(imagePokemon);
+
+    return(
+        <div>
+            {/* {renderImagePokemons} */}
+        </div>
+    )
+}
