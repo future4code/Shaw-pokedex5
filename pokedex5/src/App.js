456c79100b763ea5ee7c901ce6533c6d8a4a5e82
Matheus Augusto Moura de Lima <61365431+Mouramattheus@users.noreply.github.com>
2022-05-04 13:16:17 -0300

    global-state-hooks

--- pokedex5/src/App.js
+++ pokedex5/src/App.js
@@ -1,9 +1,16 @@
+import { GlobalState } from "./global/GlobalState";
 import Router from "./routes/Router"
 
 export default function App() {
   return (
+
+
+
+   
     <div className="App">
+       <GlobalState>
       <Router/>
+      </GlobalState>
       <p>Põe o dinoussauro na coleira! D; </p>
 
     </div>
---
+++ pokedex5/src/constants/url.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Base_URL = "https://pokeapi.co/api/v2/pokemon/"
+
+export default Base_URL
